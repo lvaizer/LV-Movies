788945db8d6bad9caa59cf66e586f693aae041c0
Liron Vaizer <lvaizer@gmail.com>
2023-03-06 11:06:10 +0200

    change environment variables name

--- src/Queries.js
+++ src/Queries.js
@@ -7,8 +7,8 @@ const TOP_MOVIES_URL = BASE_URL + 'movie/top_rated/';
 const POPULAR_MOVIES_URL = BASE_URL + 'movie/popular/';
 const NOW_PLAYING_MOVIES_URL = BASE_URL + 'movie/now_playing/';
 
-export const useGetMovie = (id) => useQuery(['movie', id], () => axios(MOVIE_URL + id + '?api_key=' + process.env.API_KEY).then(res => res.data));
-export const useGetTopRatedMovies = (start, page) => useQuery(['top_rated', page], () => axios(TOP_MOVIES_URL + '?api_key=' + process.env.API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
-export const useGetPopularMovies = (start, page) => useQuery(['popular', page], () => axios(POPULAR_MOVIES_URL + '?api_key=' + process.env.API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
-export const useGetNowPlayingMovies = (start, page) => useQuery(['nowPlaying', page], () => axios(NOW_PLAYING_MOVIES_URL + '?api_key=' + process.env.API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
+export const useGetMovie = (id) => useQuery(['movie', id], () => axios(MOVIE_URL + id + '?api_key=' + process.env.REACT_APP_API_KEY).then(res => res.data));
+export const useGetTopRatedMovies = (start, page) => useQuery(['top_rated', page], () => axios(TOP_MOVIES_URL + '?api_key=' + process.env.REACT_APP_API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
+export const useGetPopularMovies = (start, page) => useQuery(['popular', page], () => axios(POPULAR_MOVIES_URL + '?api_key=' + process.env.REACT_APP_API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
+export const useGetNowPlayingMovies = (start, page) => useQuery(['nowPlaying', page], () => axios(NOW_PLAYING_MOVIES_URL + '?api_key=' + process.env.REACT_APP_API_KEY + '&page=' + page).then(res => res.data), {enabled: start});
 
